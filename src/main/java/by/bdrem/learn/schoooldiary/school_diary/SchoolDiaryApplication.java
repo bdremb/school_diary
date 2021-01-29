81bd2ae471570b353baaed8abdcf025a40fa8b42
bdrem <bdremb@gmail.com>
2021-01-29 20:20:29 +0300

    fix some bugs

--- src/main/java/by/bdrem/learn/schoooldiary/school_diary/SchoolDiaryApplication.java
+++ src/main/java/by/bdrem/learn/schoooldiary/school_diary/SchoolDiaryApplication.java
@@ -2,10 +2,8 @@ package by.bdrem.learn.schoooldiary.school_diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SchoolDiaryApplication {
 
   public static void main(String[] args) {
