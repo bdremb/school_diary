92e712b10b49913a47a0c25e1801ae9deeb6d7b3
bdrem <bdremb@gmail.com>
2021-02-04 15:44:38 +0300

    fixed some bugs

--- src/main/java/by/bdrem/learn/schoooldiary/school_diary/config/SecurityConfiguration.java
+++ src/main/java/by/bdrem/learn/schoooldiary/school_diary/config/SecurityConfiguration.java
@@ -1,26 +1,17 @@
 package by.bdrem.learn.schoooldiary.school_diary.config;
 
-import by.bdrem.learn.schoooldiary.school_diary.model.Permission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import javax.sql.DataSource;
-
-import static by.bdrem.learn.schoooldiary.school_diary.model.Role.USER;
-import static by.bdrem.learn.schoooldiary.school_diary.model.Role.MODERATOR;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +33,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     return daoAuthenticationProvider;
   }
 
-
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(12);
@@ -61,6 +51,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
         .httpBasic();
   }
-
-
 }
--- src/main/java/by/bdrem/learn/schoooldiary/school_diary/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package by.bdrem.learn.schoooldiary.school_diary.repository;
-
-import by.bdrem.learn.schoooldiary.school_diary.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
